052a54d4197e0857755d0fcdfe18a86b0460f226
kumo <1596845544@qq.com>
2023-09-13 09:21:13 +0800

    add files, however I haven't found out how to change the MCS, maybe I should define it myself

---
+++ examples/DQN_WLAN/DQN_WLAN.cc
@@ -0,0 +1,5 @@
+//
+// Created by kumo on 9/12/23.
+//
+
+#include "DQN_WLAN.h"
---
+++ examples/DQN_WLAN/DQN_WLAN.h
@@ -0,0 +1,12 @@
+//
+// Created by kumo on 9/12/23.
+//
+
+#ifndef NS3_DQN_WLAN_H
+#define NS3_DQN_WLAN_H
+
+class DQN_WLAN
+{
+};
+
+#endif // NS3_DQN_WLAN_H
